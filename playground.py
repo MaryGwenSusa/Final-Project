573de5c6c06ac72d068c6dcdb22ad5522b4d3b0a
Mary Gwen Susa <Mary Gwen G. Susa>
2023-02-12 03:19:52 +0800

    Fixed two bugs in the house entering method by declaring a separate variable referring to set of trials and an else statement on deciding whereto enter for better security. Still, have bugs on trying to recurse the third option on security method

--- playground.py
+++ playground.py
@@ -4,6 +4,7 @@ from termcolor import cprint
 from python_play.player import play_it
 
 def houseEnter():
+    trials = 0
     attempts = ""
     turns = 3
     index = 0
@@ -37,7 +38,7 @@ def houseEnter():
                 print("You have \033[31m{}\033[0m more attempts.".format(turns))
 
                 if turns == 0:
-                    if index != 1:     
+                    if trials != 1:     
                         print("You will have attempts again after 15 mins. If you fail again, the door's alarm will ring.")
                         time.sleep(4)
                         speaker.say("Would you wait at the front door?")
@@ -56,55 +57,81 @@ def houseEnter():
                             speaker.runAndWait()
                             time.sleep(0.5)
                             turns = 3
+                            index+=1
                             pass
 
-                    if index == 1:
+                        else:
+                            trials = 1
+
+                    if trials == 1:
                         play_it('alarm.mp3')
-                        
-                        time.sleep(4)
+                        #fgdfgsgsdfg
+
+                        time.sleep(1)
                         speaker.say("Dialing 911.. Attempt of breaking in...")
                         speaker.runAndWait()
                         time.sleep(4)
                         exit()
 
                     else:
-                        index+=1
+                        trials+=1
                     #alarm? or calling 911
 
-def security():
-    doors = ['Front Door', 'Back Door']
-    #returns a zip object, which is an iterator of tuples where the first item in each passed iterator is paired together, and then the second item in each passed iterator are paired together etc.
-    doorCd = dict(zip(doors,code)) 
-    speaker.say("Do you want to reassess your security?")
-    speaker.runAndWait()
-
-    onSecurity = input("Type Y/N \n").lower()
-    # user validationhgfhfj
-    if 'y' in onSecurity:
-        speaker.say("What do you want to do?")
+def security(singleOption, askUser):
+    while singleOption == 0:
+        doors = ['Front Door', 'Back Door']
+        #returns a zip object, which is an iterator of tuples where the first item in each passed iterator is paired together, and then the second item in each passed iterator are paired together etc.
+        doorCd = dict(zip(doors,code)) 
+        speaker.say("Do you want to reassess your security?")
         speaker.runAndWait()
-        print('     1 -> Modify front door code')
-        print('     2 -> Modify back door code') #design terminal
-        time.sleep(3)
-        askUSer = int(input("Choose a number: "))
-        #user validation
-        if askUSer == 1:
-            speaker.say("Input the new code for front door")
+
+        onSecurity = input("\nType Y/N \n").lower()
+        # user validationhgfhfj
+        if 'y' in onSecurity:
+            speaker.say("What do you want to do?")
             speaker.runAndWait()
-            newCode = input("> ")
-            #from the password prog from g11dfgsdgsdg
-            doorCd["Front Door"] = newCode
-
-        time.sleep(1)
-        speaker.say('Saved!')
-        speaker.runAndWait()
-        print('Remember your new door codes:')
-        for key, value in doorCd.items():
-            print(key + " : " + value)
+            print('     \033[01m\033[36m1\033[0m -> \033[01mModify front door code\033[0m')
+            print('     \033[01m\033[36m2\033[0m -> \033[01mModify back door code\033[0m') #design terminal
+            print('     \033[01m\033[36m3\033[0m -> \033[01mModify front and back door code\033[0m')
+            time.sleep(3)
+            askUSer = int(input("Choose a number: "))
+            #user validation
+            singleOption+=1
 
         
-        
-    #elif 'n' in onSecurity:
+            if singleOption != 0:
+                if askUSer == 1:
+                    speaker.say("Input the new code for front door")
+                    speaker.runAndWait()
+                    newCode = input("> ")
+                    #from the password prog from g11dfgsdgsdg
+                    doorCd["Front Door"] = newCode
+                    
+                elif askUSer == 2:
+                    speaker.say("Input the new code for back door")
+                    speaker.runAndWait()
+                    newCode = input("> ")
+                    #from the password prog from g11dfgsdgsdg
+                    doorCd["Back Door"] = newCode
+                    
+                elif askUSer == 3:
+                    security(singleOption+1, askUSer+1)
+                    security(singleOption+1, askUSer+2)
+
+
+                #else:
+
+                time.sleep(1)
+                speaker.say('Saved!')
+                speaker.runAndWait()
+                print('Remember your new door codes:')
+                for key, value in doorCd.items():
+                    print(key + " : " + value)
+                break
+
+        #elif 'n' in onSecurity:
+
+
 # Driver program
 if __name__ == '__main__': #executes coroutine on the default event loop
     speaker = pyttsx3.init() #initialize
@@ -122,7 +149,10 @@ if __name__ == '__main__': #executes coroutine on the default event loop
     code = ['0p3nd00R', 'b@ckd00R']
 
     houseEnter()
-    security()
+
+    askUser = 0 
+    singleOption = 0 #initialize for recursion
+    security(singleOption, askUser)
     #when inside change/code
     #music?
     #check business?
