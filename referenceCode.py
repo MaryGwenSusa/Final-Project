60e2d7bbdd5ecad07594b2228a625fe091dbe176
Mary Gwen Susa <Mary Gwen G. Susa>
2023-02-11 10:22:54 +0800

    Declared variable for turns, word to be guessed and characters to be guessed

--- referenceCode.py
+++ referenceCode.py
@@ -6,3 +6,12 @@ print ("Hello, " + name, "Time to play hangman!")
 time.sleep(1)
 print ("Start guessing...")
 time.sleep(0.5)
+
+word = ("secret")
+guesses = ''
+turns = 10
+
+#check if the turns are more than zero
+while turns > 0:         
+
+    failed = 0  
