38c973dc658426c82ddad29c288c15cc503d82b4
Mary Gwen Susa <Mary Gwen G. Susa>
2023-02-11 18:51:01 +0800

    Added a timeout and continuous code if when front door was not opened. If-elif statement for front door or back door attempt. Stored the codes in a list so user can add and delete codes when inside the house

--- playground.py
+++ playground.py
@@ -1,5 +1,6 @@
 import time
 import pyttsx3
+from termcolor import cprint
 
 speaker = pyttsx3.init() #initialize
 rate = speaker.getProperty('rate')
@@ -9,38 +10,72 @@ voices = speaker.getProperty('voices')
 speaker.setProperty('voice', voices[1].id) #change voice for female through changing its index since default is male still male index is 0
 
 speaker.say("This is the front door!")
-speaker.runAndWait()
 time.sleep(1)
-print("Type in your password to unclock the door.")
+speaker.say("Type in your password to unlock the door.")
+speaker.runAndWait()
 time.sleep(0.5)
 
-code = "0p3nd00R"
+code = ['0p3nd00R', 'b@ckd00R']
 attempts = ""
 turns = 3
 
-#check if the turns are more than zero
-while turns > 0:         
-    failed = 0
+index = 0
 
-    # for every character in code    
-    for char in code:
-        if char not in attempts:
-            print("_",end=""), 
-            failed += 1
+while index:
+    #check if the turns are more than zero
+    while turns > 0:         
+        failed = 0
 
-    if failed == 0:        
-        print("\nUnlocking...")
-        time.sleep(1)
-        print("Welcome! You may come in.")
-        break
+        # for every character in code    
+        for char in code[index]:
+            if char not in attempts:
+                print("_",end=""), 
+                failed += 1
 
-    guess = input("\n") 
-    attempts += guess   
+        if failed == 0:        
+            cprint("\nUnlocking...", "green")
+            time.sleep(1)
+            speaker.say("Welcome! You may come in.")
+            speaker.runAndWait()
+            break
+            
 
-    if guess != code:    
-        turns -= 1        
-        print("Invalid.")  
-        print("You have", + turns, "more attempts")
-        print("Try again.\n", guess)
+        guess = input("\n") 
+        attempts += guess
+
+        if guess != code[index]:    
+            turns -= 1        
+            speaker.say("Invalid.")
+            speaker.runAndWait()
+            print("You have \033[31m{}\033[0m more attempts.".format(turns))
+
+            if turns == 0:           
+                print("You will have attempts again after 15 mins. If you fail again, the door's alarm will ring.")
+                #alarm? or calling 911
+                time.sleep(4)
+                speaker.say("Would you wait at the front door?")
+                speaker.runAndWait()
+                firstFail = input("Type Y/N \n").lower()
+
+                if 'y' in firstFail:
+                    turns = 3
+                    pass
+                elif 'n' in firstFail:
+                    index+=1
+                    print("You decided to go through the back door instead.")
+                    speaker.say("This is the back door!")
+                    time.sleep(1)
+                    speaker.say("Type in your password to unlock the door.")
+                    speaker.runAndWait()
+                    time.sleep(0.5)
+                    turns = 3
+                    pass
+
+
+#backdoor
+#when inside change/code
+#music?
+#check business?
+
 
 
