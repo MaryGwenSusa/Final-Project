f0fd1929905e28c274bfead861778d84bfab8127
Mary Gwen Susa <Mary Gwen G. Susa>
2023-02-11 10:32:00 +0800

    Used inner for loop for every character of the word being guessed and an if condition when character guessed correctly

--- referenceCode.py
+++ referenceCode.py
@@ -14,4 +14,9 @@ turns = 10
 #check if the turns are more than zero
 while turns > 0:         
     failed = 0
-   
+    
+    # for every character in secret_word    
+    for char in word:      
+        if char in guesses:    
+        # print then out the character
+            print (char,end=""),
