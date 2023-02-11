591fabc7d3e1281ff76f2078b5db730c4393d8fe
Mary Gwen Susa <Mary Gwen G. Susa>
2023-02-11 16:20:45 +0800

    Changed the if condition inside the for loop since I will not need the previous if condition for better security

--- playground.py
+++ playground.py
@@ -9,13 +9,13 @@ voices = speaker.getProperty('voices')
 speaker.setProperty('voice', voices[1].id) #change voice for female through changing its index since default is male still male index is 0
 
 speaker.say("This is the front door!")
-#speaker.runAndWait()
+speaker.runAndWait()
 time.sleep(1)
 print("Type in your password to unclock the door.")
 time.sleep(0.5)
 
 code = "0p3nd00R"
-guesses = ''
+attempts = ""
 turns = 3
 
 #check if the turns are more than zero
@@ -24,13 +24,8 @@ while turns > 0:
 
     # for every character in code    
     for char in code:
-        print ("_",end=""),     
-        if guesses != code:    
+        if char not in attempts:
+            print ("_",end=""), 
             failed += 1
 
-    if failed == 0:        
-        print ("\nUnlocking...")
-        time.sleep(1)
-        print ("Welcome! You may come in.")
-        break
-       
+
