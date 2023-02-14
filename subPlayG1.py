801a718ac16c71fd5cda9ed77473df580be14ed1
Mary Gwen Susa <Mary Gwen G. Susa>
2023-02-15 00:04:59 +0800

    Created dict of dicts of songs containing their details

---
+++ subPlayG1.py
@@ -0,0 +1,62 @@
+songs = {
+    "If I Had to Write a Song About You" : {
+        'Artist': 'Aaron Taylor',
+        'Genre': 'Indian Film Pop, R&B/Soul',
+        'Year': '2016',
+        'Country': 'International',
+    },
+    "Waffle Fries" : {
+        'Artist': 'sunshine blvd.',
+        'Genre': 'Pop',
+        'Year': '2019',
+        'Country': 'International',
+    },
+    "Solomon" : {
+        'Artist': 'Munimuni, Clara Benin',
+        'Genre': 'Indie Folk, Country',
+        'Year': '2019',
+        'Country': 'Local',
+    },
+    "Casual" : {
+        'Artist': 'Jesse Barrera, Jeff Bernat, Johnny Stimson',
+        'Genre': 'R&B/Soul',
+        'Year': '2020',
+        'Country': 'International',
+    },
+    "Ride" : {
+        'Artist': 'HYBS',
+        'Genre': 'Alternative/Indie, Pop',
+        'Year': '2022',
+        'Country': 'International',
+    },
+    "Absence of You" : {
+        'Artist': 'grentperez',
+        'Genre': 'Alternative/Indie, Rock',
+        'Year': '2022',
+        'Country': 'International',
+    },
+    "Too Good" : {
+        'Artist': 'Christian Kuria',
+        'Genre': 'R&B/Soul',
+        'Year': '2020',
+        'Country': 'International',
+    },
+    "Still" : {
+        'Artist': 'Jeff Bernat',
+        'Genre': 'R&B/Soul',
+        'Year': '2019',
+        'Country': 'Local',
+    },
+    "Lemonade" : {
+        'Artist': 'Jeremy Passion, Melissa Polinar, Gabe Bondoc',
+        'Genre': 'Alternative/Indie',
+        'Year': '2011',
+        'Country': 'International',
+    },
+    "Used to Me" : {
+        'Artist': 'Luke Chiang',
+        'Genre': 'Alternative/Indie, Pop',
+        'Year': '2019',
+        'Country': 'International',
+    }
+}
