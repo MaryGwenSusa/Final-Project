eacafb65101998b96b4143289a139c328de190e8
Mary Gwen Susa <Mary Gwen G. Susa>
2023-02-15 00:48:18 +0800

    Having trouble accessing the nested dict

--- subPlayG1.py
+++ subPlayG1.py
@@ -84,4 +84,15 @@ def header():
     print("=" * 33)
 
 
+
 header()
+for key in songs.keys():
+    for value in songs[key]['Artist']:
+        print('>', key, 'by', value)
+
+people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
+          2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
+
+print(people[1]['name'])
+print(people[1]['age'])
+print(people[1]['sex'])
